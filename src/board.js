06345f8b0571477aabce935b3b7ff77cd33be607
Artem Belov <aabelov1.sbt@sberbank.ru>
2017-10-04 13:10:55 +0300

    top control added

--- src/board.js
+++ src/board.js
@@ -9,29 +9,51 @@ class Board {
         this.renderPanel = this.renderPanel.bind(this);
         this.renderScreen = this.renderScreen.bind(this);
         this.dialog = new Dialog(this.renderScreen);
+        this.positionX = 0;
+        this.positionY = 0;
+
+        
     }
 
-    init () {
-        const root = document.getElementById('root');
-        const side = document.getElementById('side');
-        const left = document.getElementById('left');
+    initTop () {
+        const top = document.getElementById('top');
+        top.addEventListener('mousedown', (ev) => {
+            this.positionX = ev.clientX;
+            this.positionY = ev.clientY;
+            window.pressed = true;
+        });
+
+        top.addEventListener('mouseup', (ev) => {
+            window.pressed = false;
+        });
+
+        top.addEventListener('mouseup', (ev) => {
+            window.pressed = false;
+        });
+
+        top.addEventListener('mouseleave', (ev) => {
+            window.pressed = false;
+        })
+
+        top.addEventListener('mousemove', (ev) => {
+            const { x, y } = window.remote.getCurrentWindow().getBounds();
+            const deltaX = ev.screenX - ev.clientX;
+            const deltaY = ev.screenY - ev.clientY;
+
+            if (window.pressed) {
+                window.remote.getCurrentWindow().setPosition(
+                    x + ev.clientX - this.positionX,
+                    y + ev.clientY - this.positionY
+                )
+            }
+        })
+    }
+
+    initControls () {
         const open = document.getElementById('open');
         const minimize = document.getElementById('minimize');
         const fullscreen = document.getElementById('fullscreen');
         const quit = document.getElementById('quit');
-    
-        left.addEventListener('click', function(ev) {
-            if (ev) { ev.preventDefault(); }
-            this.pressed = !this.pressed;
-            if (this.pressed) {
-                side.style.transform = 'translateX(0%)';
-                this.innerHTML = "<";
-            } else {
-                side.style.transform = 'translateX(-100%)';    
-                this.innerHTML = ">";
-            }
-        })
-    
         open.addEventListener('click', (ev) => {
             this.dialog.openFileDialog();
             ev.stopPropagation();
@@ -56,6 +78,25 @@ class Board {
             window.remote.getCurrentWindow().close()
             ev.stopPropagation();
         });
+    }
+
+    init () {
+        this.initTop();
+        this.initControls();
+        const side = document.getElementById('side');
+        const left = document.getElementById('left');
+        
+        left.addEventListener('click', function(ev) {
+            if (ev) { ev.preventDefault(); }
+            this.pressed = !this.pressed;
+            if (this.pressed) {
+                side.style.transform = 'translateX(0%)';
+                this.innerHTML = "<";
+            } else {
+                side.style.transform = 'translateX(-100%)';    
+                this.innerHTML = ">";
+            }
+        })
     }
 
     renderPanel (files) {
